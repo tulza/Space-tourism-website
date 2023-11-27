f4d747eb31210767e6cb2cf2a32e215e01c42b17
Tulza <40633228+tulza@users.noreply.github.com>
2023-11-28 04:05:18 +1300

    init commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+<<<<<<< HEAD
+  base: "https://tulza.github.io/Space-tourism-website",
+=======
+  base: "https://tulza.github.io/url-shortening-api",
+>>>>>>> 9b2184dbf3171a6e38abc15040fd8703cceb34ba
+  plugins: [react()],
+});
